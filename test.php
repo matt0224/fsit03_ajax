8e86dfa9c3fd3a627d369bc4ed4057adab1741ce
hsinfeng <judy4a031004@gmail.com>
2017-07-29 11:13:04 +0800

    2017072901_pw1

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+    $req=$_SERVER;
+
+//    foreach($req as $key => $value){
+//       echo "{$key}={$values}<br />"
+//    }
+  echo $_SERVER['HTTP_USER_AGENT'];
+  if($agent=='matt'){
+     echo 'hello,matt'
+  }else if(strpos(#agent,'Chrome')!==false){
+  //
+     echo 'hello chrome'
+  }else{
+  echo 'hello chrome'
+  }
+?>
