8bc1104081837ae54dc2d87db689cea44d270d33
hsinfeng <judy4a031004@gmail.com>
2017-07-29 12:06:02 +0800

    2017072901_12:05

--- test.php
+++ test.php
@@ -1,16 +1,16 @@
 <?php
     $req=$_SERVER;
 
-//    foreach($req as $key => $value){
-//       echo "{$key}={$values}<br />"
-//    }
-  echo $_SERVER['HTTP_USER_AGENT'];
-  if($agent=='matt'){
-     echo 'hello,matt'
-  }else if(strpos(#agent,'Chrome')!==false){
-  //
-     echo 'hello chrome'
-  }else{
-  echo 'hello chrome'
-  }
+    foreach($req as $key => $value){
+       echo "{$key}={$values}<br />"
+    }
+//  echo $_SERVER['HTTP_USER_AGENT'];
+//  if($agent=='matt'){
+//     echo 'hello,matt'
+//  }else if(strpos(#agent,'Chrome')!==false){
+//  //
+//     echo 'hello chrome'
+//  }else{
+//  echo 'hello chrome'
+//  }
 ?>
